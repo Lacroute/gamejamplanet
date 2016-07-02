324ba02d272bf1eea156e2c242493848d2b12166
Baptiste Leproux <baptisteleproux@MacBook-Pro-de-Lucile.local>
2016-07-02 20:03:03 +0200

    requetes findpendingmessage

--- game/GameJamPlanets/Assets/Assets/Scripts/Player.cs
+++ game/GameJamPlanets/Assets/Assets/Scripts/Player.cs
@@ -12,25 +12,26 @@ public class Player{
 	}
 
 
-	public int hexid;
-
+	public int hexid; //ok
 	private List<Message> pending_messages;
-	private List<Message> send_messages;
-	Message current_message;
-	GameManager gameManagerScript;
+	//private List<Message> send_messages;
+	Message current_message; //ok
+	bool current_message_bool; //ok
+	GameManager gameManagerScript; //ok
 
 
-	public Player(int id, List<Message> pm, List<Message> sm)
+	public Player(int id, List<Message> pm, Message cm, bool b)
 	{
 		this.hexid = id;
 		this.pending_messages = pm;
-		this.send_messages = sm;
+		this.current_message = cm;
+		current_message_bool = b;
 		gameManagerScript = GameObject.Find("GameManager").GetComponent<GameManager>();
 	}
 
 	public void displayInfo()
 	{
-		Debug.Log ("Id: " + this.hexid + " / Pending messages: " + this.pending_messages + " / Send messages : " + this.send_messages);
+		Debug.Log ("Id: " + this.hexid + " / Pending messages: " + this.pending_messages + " / Current message : " + this.current_message);
 	}
 
 	public void writeMessage()
@@ -55,7 +56,7 @@ public class Player{
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(gameManagerScript.test);
+		
 	}
 
 
