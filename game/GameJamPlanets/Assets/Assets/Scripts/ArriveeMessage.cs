842f60567b7f70aada75cb62ded000b619c171e3
francoiswas <francois.cavalin@wearesocial.fr>
2016-07-03 10:32:08 +0200

    Ajout du script depart, lorsque le message quitte la planète

--- game/GameJamPlanets/Assets/Assets/Scripts/ArriveeMessage.cs
+++ game/GameJamPlanets/Assets/Assets/Scripts/ArriveeMessage.cs
@@ -3,16 +3,11 @@ using System.Collections;
 
 public class ArriveeMessage : MonoBehaviour {
 	public GameObject MessageLandTarget;
-	public GameObject MessageRotationTarget;
-	private bool RotationTargetReached = false;
-
 
 	void Update () {
 
-		if(RotationTargetReached == false){
 			transform.position = new Vector3 (Mathf.Lerp(transform.position.x,MessageLandTarget.transform.position.x,0.1F),Mathf.Lerp(transform.position.y,MessageLandTarget.transform.position.y,0.1F),Mathf.Lerp(transform.position.z,MessageLandTarget.transform.position.z,0.1F));
-		}
-			
 
 	}
 
+}
---
+++ game/GameJamPlanets/Assets/Assets/Scripts/DepartMessage.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DepartMessage : MonoBehaviour {
+	public GameObject MessageOutTarget;
+
+	void Update () {
+
+		transform.position = new Vector3 (Mathf.Lerp(transform.position.x,MessageOutTarget.transform.position.x,0.001F),Mathf.Lerp(transform.position.y,MessageOutTarget.transform.position.y,0.001F),Mathf.Lerp(transform.position.z,MessageOutTarget.transform.position.z,0.001F));
+
+	}
+}
